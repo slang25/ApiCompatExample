7c30c1b742a151f345e688a66ead5c57ba7d1821
Stuart <stuart.b.lang@gmail.com>
2019-12-28 15:56:23 +0000

    Initial commit :tada:

---
+++ MyClass.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace ApiCompatExample
+{
+public class MyClass
+{
+    public Task MyMethod(CancellationToken ct = default)
+    {
+        return Task.CompletedTask;
+    }
+}
+}
